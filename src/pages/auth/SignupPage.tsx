d68d2fa219933d0e124a3bfe03fcb41f6b5e65ad
prac2317 <lowdy92@naver.com>
2025-03-11 17:46:47 +0900

    feat: 회원가입 페이지 수정

--- src/pages/auth/SignupPage.tsx
+++ src/pages/auth/SignupPage.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignupPage = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [nickname, setNickname] = useState('');
   const [password, setPassword] = useState('');
@@ -13,20 +14,27 @@ const SignupPage = () => {
   };
 
   const signUp = async () => {
-    const response = await axios.post('http://localhost:8080/api/v1/auth/signup', {
-      email,
-      nickname,
-      password,
-    });
-
-    console.log(response);
+    try {
+      const response = await axios.post('http://localhost:8080/api/v1/auth/signup', {
+        email,
+        nickname,
+        password,
+      });
+      console.log('회원가입 성공', response);
+      navigate('/login');
+    } catch (error) {
+      console.log('회원가입 실패', error);
+    }
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
+        <h6>이메일</h6>
         <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        <h6>비밀번호</h6>
         <input value={password} onChange={(e) => setPassword(e.target.value)} />
+        <h6>닉네임</h6>
         <input value={nickname} onChange={(e) => setNickname(e.target.value)} />
         <button type="submit">회원가입</button>
       </form>
