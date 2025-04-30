3ba98679e9002c7166b7f997f5b5286866fa5e4d
prac2317 <lowdy92@naver.com>
2025-04-30 22:49:13 +0900

    chore: 배포를 위한 baseURL 주소 변경

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://3.37.123.241:8080/api',
   withCredentials: true, // 쿠키 전달 허용
 });
 
