a30ed705bb753c5db187ee14b8a87af13a538e66
prac2317 <lowdy92@naver.com>
2025-09-29 14:05:42 +0900

    feat: sse 연결 확인 및 알림 발생 시나리오 대로 알림 발생하는지 확인

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,12 +1,73 @@
 import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom';
 import { container, view } from './AppLayout.css.ts';
 import Navigation from '../components/Navigation';
+import { useEffect, useState } from 'react';
+import { fetchMemberId } from '@api/auth.ts';
 
 const AppLayout = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [unreadCount, setUnreadCount] = useState(0);
+  const [connected, setConnected] = useState(false);
+  const [memberId, setMemberId] = useState('');
+
+  useEffect(() => {
+    loadMemberId();
+  }, []);
+
+  useEffect(() => {
+    if (!memberId) {
+      return;
+    }
+    // setIsAuthenticated(true);
+    console.log('memberId AppLayout에 들어옴!!', memberId);
+  }, [memberId]);
+
+  useEffect(() => {
+    if (!memberId) {
+      return;
+    }
+
+    console.log(memberId);
+
+    const eventSource = new EventSource(
+      `${import.meta.env.VITE_BASE_URL}/api/notifications/subscribe?memberId=${memberId}`,
+      {
+        withCredentials: true,
+      },
+    );
+
+    console.log('이벤트 시작');
+
+    eventSource.addEventListener('message', (e) => {
+      console.log('message', e);
+      setUnreadCount(Number(e.data));
+    });
+
+    eventSource.addEventListener('open', (e) => {
+      console.log('open', e);
+      setConnected(true);
+    });
+
+    eventSource.addEventListener('notification', (e) => {
+      const notification = JSON.parse(e.data);
+      console.log('notification', notification);
+    });
+
+    console.log('이벤트 계속');
+  }, [memberId]);
+
+  const loadMemberId = async () => {
+    try {
+      const res = await fetchMemberId();
+      console.log(res);
+      setMemberId(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const location = useLocation();
-
   const noNavRoutes = ['/clubs/applications', '/clubs/create', '/notifications'];
-
   const showNavigation =
     !noNavRoutes.includes(location.pathname) && !location.pathname.startsWith('/chat/');
 
