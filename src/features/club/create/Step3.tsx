1e76877669cc1e2a348ad5f81fc0feceea3675bf
prac2317 <lowdy92@naver.com>
2025-12-07 17:13:57 +0900

    Refactor: 지도 입력 컴포넌트(Step3)의 타입 오류 해결 및 기타 리팩토링
    - 타입 오류 수정: 가드절을 활용한 타입 좁히기, useRef에 타입 추가, infoWindow 선언할 때 매개변수 추가
    - 변수 이름 수정: mapRef, mapInstanceRef의 이름이 비슷해서 혼동이 있을 수 있으므로 mapRef를 mapContainerRef로 변경
    - 오타 수정: mapInsanceRef를 mapInstanceRef로 변경

--- src/features/club/create/Step3.tsx
+++ src/features/club/create/Step3.tsx
@@ -7,33 +7,42 @@ const Step3 = ({ goToNextStep, formInput, setFormInput }: StepProps) => {
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
 
-  const mapRef = useRef(null);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
 
-  const mapInsanceRef = useRef(null);
-  const infoWindowInstanceRef = useRef(null);
+  const mapInstanceRef = useRef<naver.maps.Map>(null);
+  const infoWindowInstanceRef = useRef<naver.maps.InfoWindow>(null);
 
   useEffect(() => {
-    const map = new window.naver.maps.Map(mapRef.current);
-    const infoWindow = new window.naver.maps.InfoWindow();
+    if (mapContainerRef.current === null) {
+      return;
+    }
 
-    mapInsanceRef.current = map;
+    const map: naver.maps.Map = new window.naver.maps.Map(mapContainerRef.current);
+    const infoWindow: naver.maps.InfoWindow = new window.naver.maps.InfoWindow({
+      position: map.getCenter(),
+      content: '',
+    });
+
+    mapInstanceRef.current = map;
     infoWindowInstanceRef.current = infoWindow;
   }, []);
 
   function searchAddress() {
     window.naver.maps.Service.geocode({ query: address }, function (status, response) {
       if (status === window.naver.maps.Service.Status.ERROR) {
-        return alert('Something Wrong!');
+        return alert('오류가 발생했습니다.');
       }
 
+      // TODO: 주소 검색하기 기능 추가하기
       if (response.v2.meta.totalCount === 0) {
-        return alert('totalCount' + response.v2.meta.totalCount);
+        return alert('주소를 찾을 수 없습니다.');
       }
 
       var htmlAddresses = [],
         item = response.v2.addresses[0],
-        point = new window.naver.maps.Point(item.x, item.y);
+        point = new window.naver.maps.Point(Number(item.x), Number(item.y));
 
+      // TODO: 하나로 묶는 방법은 없을까?
       if (item.roadAddress) {
         htmlAddresses.push('[도로명 주소] ' + item.roadAddress);
       }
@@ -46,11 +55,15 @@ const Step3 = ({ goToNextStep, formInput, setFormInput }: StepProps) => {
         htmlAddresses.push('[영문명 주소] ' + item.englishAddress);
       }
 
-      const map = mapInsanceRef.current;
-      const infoWindow = infoWindowInstanceRef.current;
-      if (map === null || infoWindow === null) {
+      if (mapInstanceRef.current === null) {
         return;
       }
+
+      if (infoWindowInstanceRef.current === null) {
+        return;
+      }
+      const map: naver.maps.Map = mapInstanceRef.current;
+      const infoWindow: naver.maps.InfoWindow = infoWindowInstanceRef.current;
 
       if (infoWindow) {
         infoWindow.setContent(
@@ -80,7 +93,7 @@ const Step3 = ({ goToNextStep, formInput, setFormInput }: StepProps) => {
     <form className={styles.container}>
       <div className={styles.locationWrapper}>
         <h1 className={styles.title}>모임활동 장소를 정해주세요</h1>
-        <div ref={mapRef} id="map" style={{ width: '100%', height: '400px' }} />
+        <div ref={mapContainerRef} id="map" style={{ width: '100%', height: '400px' }} />
         <input className={styles.locationSearchBar} placeholder="지역을 검색해주세요." />
         <div>
           <input onChange={(e) => setAddress(e.target.value)} />
