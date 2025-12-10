85ac8f1c469e3e4b384de3f2134eadeb03443779
prac2317 <lowdy92@naver.com>
2025-12-11 01:48:58 +0900

    feat: 지역 검색 페이지에 카카오 검색 api 연동 초기 구현

--- src/features/map/LocationSearch.tsx
+++ src/features/map/LocationSearch.tsx
@@ -1,17 +1,58 @@
+import axios from "axios";
 import { useState } from "react";
+
+interface Address {
+    id: string;
+    place_name: string;
+    category_name: string;
+    category_group_code: string;
+    category_group_name: string;
+    phone: string;
+    address_name: string;
+    road_address_name: string;
+    x: string;
+    y: string;
+    place_url: string;
+    distance: string;
+}
 
 const LocationSearch = () => {
 
-    const [address, setAddress] = useState('');
+    const [addressList, setAddressList] = useState<Address[]>([]);
 
-    const searchAddress = () => {
+    const [searchWord, setSearchWord] = useState('');
 
+    const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchWord(e.target.value);
+    }
+
+    const searchAddress = async () => {
+        const response = await axios.get('https://dapi.kakao.com/v2/local/search/keyword.json', {
+            params: {
+                query: searchWord
+            },
+            headers: {
+                'Authorization': `KakaoAK ${import.meta.env.VITE_KAKAO_REST_API_KEY}`,
+            }
+        });
+
+        console.log(response.data.documents);
+
+        setAddressList(response.data.documents);
     }
 
     return (
         <div>
-            <input type="text" />
+            <input type="text" value={searchWord} onChange={onChangeSearch} />
             <button onClick={searchAddress}>검색</button>
+            <div>
+                {addressList.map((address: Address) => (
+                    <div key={address.id}>
+                        <span>{address.address_name}</span>
+                        <span>{address.place_name}</span>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
