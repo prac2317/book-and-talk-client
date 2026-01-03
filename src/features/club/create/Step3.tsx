4e1521c1bb29e7acaa6221bbaa19093768280051
prac2317 <lowdy92@naver.com>
2026-01-03 11:32:17 +0900

    refactor: Step3의 지도 표시 로직을 LocationMapView로 분리

--- src/features/club/create/Step3.tsx
+++ src/features/club/create/Step3.tsx
@@ -3,6 +3,7 @@ import { StepProps } from '@type/club.ts';
 import { useState } from 'react';
 import LocationSearch from '@features/map/LocationSearch.tsx';
 import { useMap } from '@hooks/useMap.ts'
+import LocationMapView from '@features/map/LocationMapView';
 
 const Step3 = ({ goToNextStep, formInput, setFormInput }: StepProps) => {
 
@@ -16,27 +17,16 @@ const Step3 = ({ goToNextStep, formInput, setFormInput }: StepProps) => {
         <div className={styles.locationWrapper}>
           <h1 className={styles.title}>모임활동 장소를 정해주세요</h1>
           <div style={{ display: isOpen ? 'none' : 'block' }}>
-            <input className={styles.locationSearchBar} placeholder="지역을 검색해주세요." onClick={() => { setIsOpen(true) }} />
-            <div ref={mapContainerRef} id="map" style={{ width: '100%', height: '400px' }} />
-            <div>{address?.address_name}</div>
-            <button
-              onClick={() => {
-                setFormInput({
-                  ...formInput,
-                  address: address.address_name,
-                  latitude: latitude,
-                  longitude: longitude,
-                });
-                console.log(location);
-                console.log(latitude);
-                console.log(longitude);
-                console.log(typeof latitude);
-                console.log(typeof longitude);
-              }}
-              type="button"
-            >
-              주소 입력
-            </button>
+            {/* todo: props 너무 많이 전달하는지 다시 생각해보기*/}
+            <LocationMapView
+              mapContainerRef={mapContainerRef}
+              address={address}
+              latitude={latitude}
+              longitude={longitude}
+              setIsOpen={setIsOpen}
+              formInput={formInput}
+              setFormInput={setFormInput}
+            />
           </div>
 
           {isOpen && (
