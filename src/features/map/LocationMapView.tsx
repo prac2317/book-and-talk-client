7d88f33184be12cb7e7e50abc672cf8d3873b7ca
prac2317 <lowdy92@naver.com>
2026-01-19 04:26:20 +0900

    refactor: 불필요한 의존성 삭제

--- src/features/map/LocationMapView.tsx
+++ src/features/map/LocationMapView.tsx
@@ -1,20 +1,4 @@
 import * as styles from './LocationMapView.css';
-import { FormInput } from '@type/club.ts';
-
-interface Address {
-    id: string;
-    place_name: string;
-    category_name: string;
-    category_group_code: string;
-    category_group_name: string;
-    phone: string;
-    address_name: string;
-    road_address_name: string;
-    x: string;
-    y: string;
-    place_url: string;
-    distance: string;
-}
 
 //todo: 타입 다시 정리 및 분리하기
 interface LocationMapViewProps {
